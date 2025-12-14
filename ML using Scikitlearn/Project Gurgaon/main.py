62504a31ab9db9676d699f75f22aada40f6bcb81
Hrusikesh123 <hs2068672@gmail.com>
2025-12-14 22:32:45 +0530

    final-commit

--- ML using Scikitlearn/Project Gurgaon/main.py
+++ ML using Scikitlearn/Project Gurgaon/main.py
@@ -1,3 +1,5 @@
+import os
+import joblib
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import StratifiedShuffleSplit
@@ -11,76 +13,69 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
 from sklearn.model_selection import cross_val_score
 
-# 1. Load the dataset
-housing = pd.read_csv("housing.csv")
+MODEL_FILE = "model.pkl"
+PIPELINE_FILE = "pipeline.pkl"
 
-# 2. Create a stratified test set
-housing["income_cat"] = pd.cut(
-    housing["median_income"],
-    bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
-    labels=[1, 2, 3, 4, 5])
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+def build_pipeline(num_attribs, cat_attribs):
+    # For numerical columns
+    num_pipeline = Pipeline([
+        ('imputer', SimpleImputer(strategy="median")),
+        ('scaler', StandardScaler()),
+    ])
 
-for train_index, test_index in split.split(housing, housing["income_cat"]):
-    strat_train_set = housing.loc[train_index].drop("income_cat", axis=1)
-    strat_test_set = housing.loc[test_index].drop("income_cat", axis=1)
+    # For categorical columns
+    cat_pipeline = Pipeline([
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    # Construct the full pipeline
+    full_pipeline = ColumnTransformer([
+        ("num", num_pipeline, num_attribs),
+        ("cat", cat_pipeline, cat_attribs)
+    ])
+    return full_pipeline
+
+if not os.path.exists(MODEL_FILE):
+    # Lets train the model
+    housing = pd.read_csv("housing.csv")
+
+    # Create a stratified test set
+    housing["income_cat"] = pd.cut(
+        housing["median_income"],
+        bins=[0.0, 1.5, 3.0, 4.5, 6.0, np.inf],
+        labels=[1, 2, 3, 4, 5])
+
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+    for train_index, test_index in split.split(housing, housing["income_cat"]):
+        housing.loc[test_index].drop("income_cat", axis=1).to_csv("input.csv", index=False)
+        housing = housing.loc[train_index].drop("income_cat", axis=1)
+        
+        
+    housing_labels = housing["median_house_value"].copy()
+    housing_features = housing.drop("median_house_value", axis=1)
+
+    num_attribs = housing_features.drop("ocean_proximity", axis=1).columns.tolist()
+    cat_attribs = ["ocean_proximity"]
+
+    pipeline = build_pipeline(num_attribs, cat_attribs)
+    housing_prepared = pipeline.fit_transform(housing_features)
     
-# We will work on the copy of the training dataset
-housing = strat_train_set.copy()
+    model = RandomForestRegressor(random_state=42)
+    model.fit(housing_prepared, housing_labels)
 
-# 3. Separate features and labels
-housing_labels = housing["median_house_value"].copy()
-housing = housing.drop("median_house_value", axis=1)
+    joblib.dump(model, MODEL_FILE)
+    joblib.dump(pipeline, PIPELINE_FILE)
+    print("Model is trained. Congrats!")
+else:
+    # Lets do inference
+    model = joblib.load(MODEL_FILE)
+    pipeline = joblib.load(PIPELINE_FILE)
 
-print(housing, housing_labels)
+    input_data = pd.read_csv('input.csv')
+    transformed_input = pipeline.transform(input_data)
+    predictions = model.predict(transformed_input)
+    input_data['median_house_value'] = predictions
 
-# 4. Separate numerical and categorical columns
-num_attribs = housing.drop("ocean_proximity", axis=1).columns.tolist()
-cat_attribs = ["ocean_proximity"]
-
-# 5. Lets make the pipeline 
-
-# For numerical columns
-num_pipeline = Pipeline([
-    ('imputer', SimpleImputer(strategy="median")),
-    ('scaler', StandardScaler()),
-])
-
-# For categorical columns
-cat_pipeline = Pipeline([
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
-
-# Construct the full pipeline
-full_pipeline = ColumnTransformer([
-    ("num", num_pipeline, num_attribs),
-    ("cat", cat_pipeline, cat_attribs)
-])
-
-# 6. Transform the data
-housing_prepared = full_pipeline.fit_transform(housing)
-print(housing_prepared.shape)
-
-# 7. Train the model
-# Linear Regression Model
-lin_reg = LinearRegression()
-lin_reg.fit(housing_prepared, housing_labels)
-lin_preds = lin_reg.predict(housing_prepared)
-lin_rmse = root_mean_squared_error(housing_labels, lin_preds)
-print(f"The root mean squared error for Linear Regression is: {lin_rmse}")
-
-# Decision Tree Regressor
-dec_reg = DecisionTreeRegressor()
-dec_reg.fit(housing_prepared, housing_labels)
-dec_preds = dec_reg.predict(housing_prepared)
-# dec_rmse = root_mean_squared_error(housing_labels, dec_preds)
-dec_rmses = -cross_val_score(dec_reg, housing_prepared, housing_labels, scoring="neg_root_mean_squared_error", cv=10)
-# print(f"The root mean squared error for Decision Tree is: {dec_rmses}")
-print(pd.Series(dec_rmses).describe())
-
-# Random Forest Regressor
-random_forest_reg = RandomForestRegressor()
-random_forest_reg.fit(housing_prepared, housing_labels)
-random_forest_preds = random_forest_reg.predict(housing_prepared)
-random_forest_rmse = root_mean_squared_error(housing_labels, random_forest_preds)
-print(f"The root mean squared error for Random Forest is: {random_forest_rmse}")
+    input_data.to_csv('output.csv', index=False)
+    print("Inference completed, results saved to output.csv Enjoy!")
